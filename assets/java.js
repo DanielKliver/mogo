3d37c070ada4929719cf06fceaf61c7b6015aca6
Mihael Yaryomenko <mihaelyaryomenko2005@gmail.com>
2023-12-12 00:19:44 +0700

    it works, but I am sorry

--- assets/java.js
+++ assets/java.js
@@ -1,7 +1,7 @@
-var dialog = document.querySelector('dialog')
-document.querySelector('#open').onclick = function () {
-  dialog.showModal()
-}
+// var dialog = document.querySelector('dialog')
+// document.querySelector('#open').onclick = function () {
+//   dialog.showModal()
+// }
 
 function before() {
   document.getElementById('message').innerHTML =
@@ -13,32 +13,35 @@ function afterr() {
     '“Я Джон и я Эмо. Однажды и тебе представится возможность оказаться на сайте в образе чувака, который толкает цитаты, но эмо тебе никогда не стать”'
 }
 
-const switchSourceOfImage = () => {
+const switchSourceOfImage = (() => {
   const imageToChange = document.querySelector('#mainImage')
   const sourceOfImg1 = 'assets/images/about/1.jpg'
   const sourceOfImg2 = 'assets/images/about/2.jpg'
   const sourceOfImg3 = 'assets/images/about/3.jpg'
+
   const switchForward = () => {
-    if (imageToChange.src === sourceOfImg1) {
+    const filename = imageToChange.src.split('/').pop()
+    if (filename === '1.jpg') {
       imageToChange.src = sourceOfImg2
-    } else if (imageToChange.src === sourceOfImg2) {
+    } else if (filename === '2.jpg') {
       imageToChange.src = sourceOfImg3
     } else {
       imageToChange.src = sourceOfImg1
     }
   }
+
   const switchBackward = () => {
-    if (imageToChange.src === sourceOfImg3) {
+    const filename = imageToChange.src.split('/').pop()
+    if (filename === '3.jpg') {
       imageToChange.src = sourceOfImg2
-    } else if (imageToChange.src === sourceOfImg2) {
+    } else if (filename === '2.jpg') {
       imageToChange.src = sourceOfImg1
     } else {
       imageToChange.src = sourceOfImg3
     }
   }
   const buttonForward = document.querySelector('#forward')
-  buttonForward.onclick(switchForward)
-  const buttonBackward = document.querySelector('#backward')
-  buttonBackward.onclick(switchBackward)
-  return { switchForward, switchBackward }
-}
+  buttonForward.addEventListener('click', switchForward)
+  const buttonBackward = document.querySelector('#backwards')
+  buttonBackward.addEventListener('click', switchBackward)
+})()
