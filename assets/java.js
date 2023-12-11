1c1ee1590c0e451951cf1b59ed3773b4bc52632e
DanielKliver <povishevd@mail.ru>
2023-12-11 18:36:16 +0700

    version_1

---
+++ assets/java.js
@@ -0,0 +1,22 @@
+
+  var dialog = document.querySelector("dialog")
+  document.querySelector("#open").onclick = function () {
+    dialog.showModal()
+  }
+
+
+
+
+function before(){
+    
+    document.getElementById('message')
+    .innerHTML="“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.”";
+}
+     
+function afterr(){
+    
+    document.getElementById('message')
+    .innerHTML="“Я Джон и я Эмо. Однажды и тебе представится возможность оказаться на сайте в образе чувака, который толкает цитаты, но эмо тебе никогда не стать”";
+}
+
+ 
